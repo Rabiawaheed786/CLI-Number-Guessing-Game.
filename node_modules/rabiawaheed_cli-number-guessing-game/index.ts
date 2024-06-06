f991c0c52484695ba9659dbcebafff9eafec4a85
Rabia waheed <Ahtishamwaheed0786@gmail.com>
2024-06-06 22:18:56 +0400

    Addiding a Number guessing game

---
+++ node_modules/rabiawaheed_cli-number-guessing-game/index.ts
@@ -0,0 +1,26 @@
+#!/ usr/bin/env node
+
+import { log } from "console";
+import inquirer from "inquirer";
+
+console.log("\n\twelcome to Rabiawaheed -cli Number guessing Game");
+
+const randomNumber = Math.floor(Math.random() * 5 + 1);
+
+const answer = await inquirer.prompt([
+    {
+        name: "userguessedNumber",
+
+        type: "number",
+
+        message: "Enter your guessed number(Number limit from 1 t0 5:)",
+
+    },
+]);
+
+if(answer.userguessedNumber === randomNumber ){
+    console.log("congratulation !you guess a correct Number" );
+
+}else{
+    console.log("sorry,you guess a wrong Number");``
+}
